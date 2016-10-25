7e339713fbbf8180f921fc28d41b7db02d7dfff8
Tran Van Tuan B <tran.van.tuanb@framgia.com>
2016-10-25 11:43:57 +0700

    Filling in the layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
   root 'static_page#home'
-  get 'static_page/home'
-  get 'static_page/help'
-  get 'static_page/about'
-  get 'static_page/contact'
+  get  '/help',    to: 'static_page#help'
+  get  '/about',   to: 'static_page#about'
+  get  '/contact', to: 'static_page#contact'
+  get  '/signup',  to: 'users#new'
 end
