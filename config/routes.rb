da0524e7f033dbdb5663b0d3ee6b7eb76bf62b63
Tran Van Tuan B <tran.van.tuanb@framgia.com>
2016-10-26 14:22:23 +0700

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
   get  '/about',   to: 'static_page#about'
   get  '/contact', to: 'static_page#contact'
   get  '/signup',  to: 'users#new'
+  resources :users
 end
