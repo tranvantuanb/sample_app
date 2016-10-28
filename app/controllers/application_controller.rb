aefa684cbda9777491fc2f6baa37ef23b0a364b2
Tran Van Tuan B <tran.van.tuanb@framgia.com>
2016-10-28 13:59:33 +0700

    Finish user edit, update, index, and destroy actions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,25 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
   include ApplicationHelper
+
   def render_404
     render file: "#{Rails.root}/public/404.html", layout: false, status: 404
+  end
+
+  def verify_user
+    unless logged_in?
+      store_location
+      flash[:danger] = t ".please_log_in"
+      redirect_to login_url
+    end
+  end
+
+  def correct_user
+    load_user
+    redirect_to root_url unless current_user? @user
+  end
+
+  def verify_admin
+    redirect_to root_url unless current_user.admin?
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,10 +7,10 @@ class User < ApplicationRecord
     format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
 
   has_secure_password
-  validates :password, presence: true, length: {minimum: 6}
+  validates :password, presence: true, length: {minimum: 6}, allow_nil: true
 
   class << self
-    def digest(string)
+    def digest string
       cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST
         : BCrypt::Engine.cost
       BCrypt::Password.create string, cost: cost
