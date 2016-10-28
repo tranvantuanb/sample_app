79985da71dcd8dd229068884f33af8564183f265
Tran Van Tuan B <tran.van.tuanb@framgia.com>
2016-10-28 13:57:21 +0700

    Finish user edit, update, index, and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,15 @@
 class UsersController < ApplicationController
+  before_action :verify_user, except: [:new, :create, :show]
+  before_action :correct_user, only: [:edit, :update]
+  before_action :verify_admin, only: :destroy
+  before_action :load_user, except: [:new, :index, :create]
+
+  def index
+    @users = User.select(:name, :email, :id)
+      .paginate(page: params[:page], per_page: Settings.page_size).order "name ASC"
+  end
+
   def show
-    @user = User.find_by id: params[:id]
-    render_404 unless @user
   end
 
   def new
@@ -19,9 +27,33 @@ class UsersController < ApplicationController
     end
   end
 
-  private
-    def user_params
-      params.require(:user).permit :name, :email, :password,
-        :password_confirmation
+  def edit
+  end
+
+  def update
+    if @user.update_attributes user_params
+      flash[:success] = t ".profile_updated"
+      redirect_to @user
+    else
+      flash.now[:danger] = t ".update_failed"
+      render :edit
     end
+  end
+
+  def destroy
+    @user.destroy
+    flash[:success] = t ".user_deleted"
+    redirect_to users_url
+  end
+
+  private
+  def user_params
+    params.require(:user).permit :name, :email, :password,
+      :password_confirmation
+  end
+
+  def load_user
+    @user = User.find_by id: params[:id]
+    render_404 unless @user
+  end
 end
