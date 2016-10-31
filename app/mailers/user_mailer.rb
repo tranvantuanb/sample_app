6060ff73008997a64f96f898ce162dbc60ff0331
Tran Van Tuan B <tran.van.tuanb@framgia.com>
2016-10-31 10:43:05 +0700

    Add password reset

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,9 +5,8 @@ class UserMailer < ApplicationMailer
     mail to: user.email, subject: t(".account_activation")
   end
 
-  def password_reset
-    @greeting = t(".hi")
-
-    mail to: t(".to")
+  def password_reset user
+    @user = user
+    mail to: user.email, subject: t(".password_reset")
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get "password_resets/new"
+
+  get "password_resets/edit"
+
   root "static_page#home"
   get "/help", to: "static_page#help"
   get "/about", to: "static_page#about"
@@ -9,4 +13,5 @@ Rails.application.routes.draw do
   delete "/logout", to: "sessions#destroy"
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets, except: [:index, :show, :destroy]
 end
